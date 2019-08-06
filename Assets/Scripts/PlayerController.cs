1dca6b3de8067edb714a2cc35533f739aec87a16
marsrvr <marsrvr@gmail.com>
2019-08-06 10:51:19 -0700

    Brining project under management as coding begins.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed; // character speed.
+    private Rigidbody2D rb; // character body
+    private float moveInputH; // control input Horizontal
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Updates physics engine.
+    private void FixedUpdate()
+    {  // -1 is left, 0 still, 1 is right
+        moveInputH = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * moveInputH, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

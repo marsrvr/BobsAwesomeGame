8d036ba76f97a51081744d3321209fb1090beaf7
marsrvr <marsrvr@gmail.com>
2019-08-06 12:48:13 -0700

    Added jump state reset by ground collisions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed;       // character speed.
     public float jumpForce;   // How much are we jumping?
 
+    public bool isJumping;    // jumping state
+
     private Rigidbody2D rb;   // character body
 
     private float moveInputH; // control input Horizontal
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isJumping = false;
     }
 
     // Updates physics engine.
@@ -28,11 +31,21 @@ public class PlayerController : MonoBehaviour
 
         // -1 is down, 0 is still, 1 is up
         jumpInputV = Input.GetAxis("Vertical");
-        if (jumpInputV > 0) // is a float
+        if (jumpInputV > 0 && isJumping == false) // is a float
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
         }
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
     }
 
     // Update is called once per frame

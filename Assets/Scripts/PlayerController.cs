1e904e60ed300dc01a4b0df0c5c37a976dec6402
marsrvr <marsrvr@gmail.com>
2019-08-06 11:19:05 -0700

    Added code to enable basic jumping with up arrow. Controls are force-based floating points. Can jump mid-air and forever, so...not realistic yet.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed; // character speed.
-    private Rigidbody2D rb; // character body
-    private float moveInputH; // control input Horizontal
+    public float speed;       // character speed.
+    public float jumpForce;   // How much are we jumping?
 
+    private Rigidbody2D rb;   // character body
+
+    private float moveInputH; // control input Horizontal
+    private float jumpInputV; // control jump input vertical
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +21,18 @@ public class PlayerController : MonoBehaviour
 
     // Updates physics engine.
     private void FixedUpdate()
-    {  // -1 is left, 0 still, 1 is right
+    {  
+        // -1 is left, 0 still, 1 is right
         moveInputH = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed * moveInputH, rb.velocity.y);
+
+        // -1 is down, 0 is still, 1 is up
+        jumpInputV = Input.GetAxis("Vertical");
+        if (jumpInputV > 0) // is a float
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
     }
 
     // Update is called once per frame
